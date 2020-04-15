79b0fb060a8b7b10022e242e8e3151b42a8a62ad
Cem Topkaya <cem.topkaya@hotmail.com>
2020-04-15 08:57:54 +0300

    hede

--- src/tr/com/cinar/SCM.groovy
+++ src/tr/com/cinar/SCM.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/env groovy
 // com/cleverbuilder/GlobalVars.groovy
-package com.cleverbuilder
+package com.cinar
 
 class SCM { 
+
     String URL = "ssh"
     String CredId = "...."
 
@@ -17,11 +18,11 @@ class SCM {
     }
 
     def injectVars() {
-        steps.env.PROJECT = steps.sh(script: 'echo $JOB_NAME | cut -d "/" -f 1', returnStdout: true).trim()
-        steps.env.REPO = steps.sh(script: 'echo $JOB_NAME | cut -d "/" -f 2', returnStdout: true).trim()
-        steps.env.SHORT_SHA = steps.sh(script: 'echo $GIT_COMMIT | cut -c 1-10', returnStdout: true).trim()
-        steps.env.TIME = steps.sh(script: 'date +%Y-%m-%d_%H%M%S', returnStdout: true).trim()
-        steps.env.TAG = steps.sh(script: 'echo ${BRANCH_NAME}-${SHORT_SHA} | sed "s+/+-+g"', returnStdout: true).trim()
+        // steps.env.PROJECT = steps.sh(script: 'echo $JOB_NAME | cut -d "/" -f 1', returnStdout: true).trim()
+        // steps.env.REPO = steps.sh(script: 'echo $JOB_NAME | cut -d "/" -f 2', returnStdout: true).trim()
+        // steps.env.SHORT_SHA = steps.sh(script: 'echo $GIT_COMMIT | cut -c 1-10', returnStdout: true).trim()
+        // steps.env.TIME = steps.sh(script: 'date +%Y-%m-%d_%H%M%S', returnStdout: true).trim()
+        // steps.env.TAG = steps.sh(script: 'echo ${BRANCH_NAME}-${SHORT_SHA} | sed "s+/+-+g"', returnStdout: true).trim()
     }    
    
    
